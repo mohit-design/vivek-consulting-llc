219cb935d1a0cb34a3c718dabfe126852163a3c8
Mohit Patel <45488208+mohit-design@users.noreply.github.com>
2024-07-11 09:45:50 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,82 @@
 $(document).ready(function() {
-  
+  // About Us Section GreenSock Animation Platform JS
+  const whoWeAreMainHeadingTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#whoWeAreMainHeading",
+      start: "top 75%",
+      end: "bottom 0%",
+      onToggle: self => gsap.to(".who-we-are-main-heading", {opacity: self.isActive ? 1 : 0}),
+      toggleActions: "restart pause restart none"
+    }
+  });
+  const whoWeAreMainHeadingSplit = new SplitText(".who-we-are-main-heading");
+  whoWeAreMainHeadingTimeline.from(whoWeAreMainHeadingSplit.chars, {
+    duration: 0.6,
+    scale: 1,
+    autoAlpha: 0,
+    rotationX: -90,
+    transformOrigin: "100% 50%",
+    ease: "back",
+    stagger: 0.05
+  });
+  // Services Section GreenSock Animation Platform JS
+  const servicesMainHeadingTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#servicesMainHeading",
+      start: "top 75%",
+      end: "bottom 0%",
+      onToggle: self => gsap.to(".services-main-heading", {opacity: self.isActive ? 1 : 0}),
+      toggleActions: "restart pause restart none"
+    }
+  });
+  const servicesMainHeadingSplit = new SplitText(".services-main-heading");
+  servicesMainHeadingTimeline.from(servicesMainHeadingSplit.chars, {
+    duration: 0.6,
+    scale: 1,
+    autoAlpha: 0,
+    rotationX: -90,
+    transformOrigin: "100% 50%",
+    ease: "back",
+    stagger: 0.05
+  });
+  // Why Choose Us Section GreenSock Animation Platform JS
+  const whyChooseUsMainHeadingTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#whyChooseUsMainHeading",
+      start: "top 75%",
+      end: "bottom 0%",
+      onToggle: self => gsap.to(".why-choose-us-main-heading", {opacity: self.isActive ? 1 : 0}),
+      toggleActions: "restart pause restart none"
+    }
+  });
+  const whyChooseUsMainHeadingSplit = new SplitText(".why-choose-us-main-heading");
+  whyChooseUsMainHeadingTimeline.from(whyChooseUsMainHeadingSplit.chars, {
+    duration: 0.6,
+    scale: 1,
+    autoAlpha: 0,
+    rotationX: -90,
+    transformOrigin: "100% 50%",
+    ease: "back",
+    stagger: 0.05
+  });
+  // Blogs Section GreenSock Animation Platform JS
+  const blogsMainHeadingTimeline = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#blogsMainHeading",
+      start: "top 75%",
+      end: "bottom 0%",
+      onToggle: self => gsap.to(".blogs-main-heading", {opacity: self.isActive ? 1 : 0}),
+      toggleActions: "restart pause restart none"
+    }
+  });
+  const blogsMainHeadingSplit = new SplitText(".blogs-main-heading");
+  blogsMainHeadingTimeline.from(blogsMainHeadingSplit.chars, {
+    duration: 0.6,
+    scale: 1,
+    autoAlpha: 0,
+    rotationX: -90,
+    transformOrigin: "100% 50%",
+    ease: "back",
+    stagger: 0.05
+  });
 });
